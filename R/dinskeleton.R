c007946f08e8003787e5f638ad3d4202a6a3fca8
ansesi <tao281168@gmail.com>
2022-06-23 16:22:25 -0300

    Initial commit

---
+++ R/dinskeleton.R
@@ -0,0 +1,24 @@
+---
+    title: "Taxa de Queda com Dano"
+author:
+    - name: "Núcleo de Segurança do Paciente"
+- name: "Sistema de Gestão da Qualidade"
+affiliation: "Prevent Senior"
+abstract: "Este relatório estabelece diretrizes para implementar um modelo de ajuste de risco de queda hospitalar na Rede Hospitalar Sancta Maggiore com base em fatores de risco de queda relacionados ao paciente"
+
+date: "2022-06-16"
+output:
+    prettydoc::html_pretty:
+    theme: leonids
+highlight: github
+#knit: pagedown::chrome_print
+bibliography:
+    - references/sihosp.bib
+- references/packages.bib
+csl: references/abnt.csl
+link-citations: TRUE
+---
+
+    ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
